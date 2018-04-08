b580325c78b564615b81abb0aa4b31033b135e98
dmicanzerofox <dmican@zerofox.com>
2018-04-07 23:28:41 -0400

    TestCoverage: Subprocess execute

--- actions/shell_test.go
+++ actions/shell_test.go
@@ -1,7 +1,37 @@
 package actions
 
-import "testing"
+import (
+	"testing"
+	"context"
+	"github.com/dm03514/test-engine/results"
+)
 
 func TestSubprocess_applyOverrides_(t *testing.T) {
 	t.Skip()
+}
+
+func TestSubprocessResult_ValueOfProperty(t *testing.T) {
+	t.Skip()
+}
+
+
+func TestSubprocess_Execute(t *testing.T) {
+	s := Subprocess{
+		CommandName: "printf",
+		Args: []string{"hello"},
+	}
+
+	r, err := s.Execute(context.Background(), results.Results{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	sr := r.(SubprocessResult)
+	if string(sr.Output) != "hello" {
+		t.Errorf("Expected %q received %q", "hello", sr.Output)
+	}
+	if sr.Returncode != 0 {
+		t.Errorf("Expected returncode 0, received: %q", sr.Returncode)
+	}
+
 }
